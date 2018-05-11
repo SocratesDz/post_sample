e9c5f78c2d897a8a92e521dab441835c20ef42e9
Sócrates Díaz <sdsxd1800@gmail.com>
2018-05-10 23:35:19 -0400

    Added missing files in previous commit.

---
+++ lib/screens/pictures.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PicturesPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _PicturesPageState();
+}
+
+class _PicturesPageState extends State<PicturesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold();
+  }
+}
