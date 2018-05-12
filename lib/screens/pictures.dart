989300ae8ececfdaf2637d51b3be03d0b9c36b38
Sócrates Díaz <sdsxd1800@gmail.com>
2018-05-12 19:17:56 -0400

    Added pictures section. Created slider section.

--- lib/screens/pictures.dart
+++ lib/screens/pictures.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:posts_sample/api.dart';
+import 'package:posts_sample/models.dart';
+import 'package:posts_sample/screens/picture_slider.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class PicturesPage extends StatefulWidget {
   @override
@@ -8,6 +12,43 @@ class PicturesPage extends StatefulWidget {
 class _PicturesPageState extends State<PicturesPage> {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold();
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text("Pictures"),
+      ),
+      body: FutureBuilder(
+        future: Api.fetchPictures(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text("No photos :("));
+          } else if (snapshot.hasData) {
+            final pictures = snapshot.data as List<Picture>;
+            return GridView.count(
+                crossAxisCount: 2,
+                children: _generatePictureTiles(pictures));
+          }
+          return Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
   }
+
+  List<Widget> _generatePictureTiles(List<Picture> pictures) {
+    return pictures.asMap().map((index, picture) =>
+        MapEntry(index, GridTile(
+    child: new InkWell(
+      onTap: () => Navigator.push(context,
+          MaterialPageRoute(
+              builder: (context) =>
+              PictureSlider(pictures: pictures, indexPicture: index))),
+      child: CachedNetworkImage(
+        placeholder: Center(
+            heightFactor: 0.5,
+            widthFactor: 0.5,
+            child: CircularProgressIndicator()),
+        imageUrl: picture.thumbnailUrl,
+        fit: BoxFit.cover,
+      ),
+    ),))).values.toList();
+    }
 }
